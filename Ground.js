e9e89ea3d4736ae40959a7080dd03c5d398257aa
shivaguru2007-coder <lakshmikodi1982@gmail.com>
2020-09-17 22:20:09 +0530

    f

--- Ground.js
+++ Ground.js
@@ -1,7 +1,11 @@
 class Ground {
     constructor(x,y,width,height) {
       var options = {
-          isStatic: true
+
+          isStatic: true,
+          restitution:0.3,
+          friction:0.5,
+          density:1.2
       }
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
